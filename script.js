cc8552014b14c8fe1d071942c6256ed4c9444e23
PedroHenrique34 <pedrohenriquesousa0512@gmail.com>
2024-03-02 17:20:08 -0300

    alter

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function insert(num){
-    let numero = documentgetElementById("resultado").innerHTML;
-    documentgetElementById("resultado").innerHTML = numero + num ;
+    let numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num ;
     
+}
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
+function back(){
+    let result = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML =result.substring(0,result.length - 1);
+}
+function calcular(){
+    let calcule= document.getElementById('resultado').innerHTML;
+    if(calcule){
+        document.getElementById('resultado').innerHTML= eval(calcule);
+    }
+    else{
+        document.getElementById('resultado').innerHTML= 'Nada encontrado'
+    }
 }
